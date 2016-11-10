098b86fd4b80b66ade7c881027bb374067fd161b
sajithajp <sajithajp@gmail.com>
2016-11-10 15:21:34 +0430

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,30 +4,30 @@
 ## Note: The input matrix should be invertible.
 
 makeCacheMatrix <- function(x = matrix()) {
-m<-NULL
-set<-function(y){
-x<<-y
-m<<-NULL
-}
-get<-function() x
-setinverse<-function(value) m <<-value
-getinverse<-function() m
-list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
-}
-
+ m<-NULL
+ set<-function(y){
+ x<<-y
+ m<<-NULL
+ }
+ get<-function() x
+ setinverse<-function(value) m <<-value
+ getinverse<-function() m
+ list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+ }
 
 ## This function computes the inverse of the special object matrix from the above function 
 ##  If the inverse has already been calculated then it will retrive the inverse from the above created cache
-       
-cacheSolve<-function(x,...){
-       m<-x$getinverse()
-if(!is.null(m)) {
-message("getting cache data")
-return(m)
-}
-data<-x$get()
-m<-solve(data,...)
-x$setinverse(m)
-m
 
+cacheSolve <- function(x, ...) {
+         m<-x$getinverse()
+ if(!is.null(m)) {
+ message("getting cache data")
+ return(m)
+ }
+ data<-x$get()
+ m<-solve(data,...)
+ x$setinverse(m)
+ m
+ 
 }
+
